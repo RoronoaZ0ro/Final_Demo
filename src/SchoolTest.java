5da86ecdf9d8e1a6b18365a6a8eeef02381eb196
UnMaple <UnMaple@DESKTOP-ASU3I1C>
2019-06-08 12:32:28 +0800

    Update SchoolTest

--- src/SchoolTest.java
+++ src/SchoolTest.java
@@ -9,60 +9,132 @@ public class SchoolTest {
 
 	@Test
 	public void test() {
+		System.out.println("Test1:");
 		School sc1 = new School("台大", 2, 1);
 		
 		String School_list[] = {"台大","清大"};
-		Student st1 = new Student("Test", 80, School_list);
-
-		for(int i = 0; i < st1.getSchool_list().length; i++) {
-			if(sc1.getSchool_name().equals(st1.getSchool(i))) {
-				sc1.send_Score(st1.getStudent_name(), st1.getScore());
-			}
-		}
+		Student st1 = new Student("Student_1", 80, School_list);
 		
 		String School_list_2[] = {"台大"};
-		Student st2 = new Student("Test2", 70, School_list_2);
-
-		for(int i = 0; i < st2.getSchool_list().length; i++) {
-			if(sc1.getSchool_name().equals(st2.getSchool(i))) {
-				sc1.send_Score(st2.getStudent_name(), st2.getScore());
-			}
-		}
+		Student st2 = new Student("Student_2", 70, School_list_2);
 		
 		String School_list_3[] = {"台大"};
-		Student st3 = new Student("Test3", 80, School_list_3);
-
-		for(int i = 0; i < st3.getSchool_list().length; i++) {
-			if(sc1.getSchool_name().equals(st3.getSchool(i))) {
-				sc1.send_Score(st3.getStudent_name(), st3.getScore());
-			}
-		}
+		Student st3 = new Student("Student_3", 80, School_list_3);
 		
 		String School_list_4[] = {"台大"};
-		Student st4 = new Student("Test4", 60, School_list_4);
-
-		for(int i = 0; i < st4.getSchool_list().length; i++) {
-			if(sc1.getSchool_name().equals(st4.getSchool(i))) {
-				sc1.send_Score(st4.getStudent_name(), st4.getScore());
-			}
-		}
+		Student st4 = new Student("Student_4", 60, School_list_4);
 		
 		String School_list_5[] = {"台大"};
-		Student st5 = new Student("Test5", 79, School_list_5);
-
-		for(int i = 0; i < st5.getSchool_list().length; i++) {
-			if(sc1.getSchool_name().equals(st5.getSchool(i))) {
-				sc1.send_Score(st5.getStudent_name(), st5.getScore());
+		Student st5 = new Student("Student_5", 79, School_list_5);
+		
+		Student[] Students = {st1,st2,st3,st4,st5};
+		
+		for(int i = 0;i<Students.length;i++) {
+			for(int j = 0; j < Students[i].getSchool_list().length; j++) {
+				if(sc1.getSchool_name().equals(Students[i].getSchool(j))) {
+					sc1.send_Score(Students[i].getStudent_name(), Students[i].getScore());
+				}
 			}
 		}
 		
 		List<String> Result_list = new ArrayList<String>();
-		Result_list.add("Test" + " Primary");
-		Result_list.add("Test3" + " Primary");
-		Result_list.add("Test5" + " Secondary");
-		Result_list.add("Test2" + " None");
-		Result_list.add("Test4" + " None");
+		Result_list.add("Student_1" + " Primary");
+		Result_list.add("Student_3" + " Primary");
+		Result_list.add("Student_5" + " Secondary");
+		Result_list.add("Student_2" + " None");
+		Result_list.add("Student_4" + " None");
 		assertEquals(Result_list,sc1.Release());
+	}
+	
+	@Test
+	public void test2() {
+		System.out.println();
+		System.out.println("Test2:");
+		School sc1 = new School("台大", 0, 0);
+		
+		String School_list[] = {"台大","清大"};
+		Student st1 = new Student("Student_1", 80, School_list);
+		
+		String School_list_2[] = {"台大"};
+		Student st2 = new Student("Student_2", 70, School_list_2);
+		
+		String School_list_3[] = {"台大"};
+		Student st3 = new Student("Student_3", 80, School_list_3);
+		
+		String School_list_4[] = {"台大"};
+		Student st4 = new Student("Student_4", 60, School_list_4);
+		
+		String School_list_5[] = {"台大"};
+		Student st5 = new Student("Student_5", 79, School_list_5);
+		
+		Student[] Students = {st1,st2,st3,st4,st5};
+		
+		for(int i = 0;i<Students.length;i++) {
+			for(int j = 0; j < Students[i].getSchool_list().length; j++) {
+				if(sc1.getSchool_name().equals(Students[i].getSchool(j))) {
+					sc1.send_Score(Students[i].getStudent_name(), Students[i].getScore());
+				}
+			}
+		}
+		
+		List<String> Result_list = new ArrayList<String>();
+		Result_list.add("Student_1" + " None");
+		Result_list.add("Student_3" + " None");
+		Result_list.add("Student_5" + " None");
+		Result_list.add("Student_2" + " None");
+		Result_list.add("Student_4" + " None");
+		assertEquals(Result_list,sc1.Release());
+	}
+	
+	@Test
+	public void test3() {
+		System.out.println();
+		System.out.println("Test3:");
+		School sc1 = new School("台大", 2, 1);
+		School sc2 = new School("清大", 1, 2);
+		
+		String School_list[] = {"台大","清大"};
+		Student st1 = new Student("Student_1", 80, School_list);
+		
+		String School_list_2[] = {"台大"};
+		Student st2 = new Student("Student_2", 70, School_list_2);
+		
+		String School_list_3[] = {"台大"};
+		Student st3 = new Student("Student_3", 80, School_list_3);
+		
+		String School_list_4[] = {"台大"};
+		Student st4 = new Student("Student_4", 60, School_list_4);
+		
+		String School_list_5[] = {"台大"};
+		Student st5 = new Student("Student_5", 79, School_list_5);
+		
+		Student[] Students = {st1,st2,st3,st4,st5};
+		
+		for(int i = 0;i<Students.length;i++) {
+			for(int j = 0; j < Students[i].getSchool_list().length; j++) {
+				if(sc1.getSchool_name().equals(Students[i].getSchool(j))) {
+					sc1.send_Score(Students[i].getStudent_name(), Students[i].getScore());
+				}
+				if(sc2.getSchool_name().equals(Students[i].getSchool(j))) {
+					sc2.send_Score(Students[i].getStudent_name(), Students[i].getScore());
+				}
+			}
+		}
+		
+		List<String> Result_list = new ArrayList<String>();
+		Result_list.add("Student_1" + " Primary");
+		Result_list.add("Student_3" + " Primary");
+		Result_list.add("Student_5" + " Secondary");
+		Result_list.add("Student_2" + " None");
+		Result_list.add("Student_4" + " None");
+		
+		List<String> Result_list2 = new ArrayList<String>();
+		Result_list2.add("Student_1" + " Primary");
+		
+		assertEquals(Result_list,sc1.Release());
+		assertEquals(Result_list2,sc2.Release());
+		
+		
 	}
 
 }
